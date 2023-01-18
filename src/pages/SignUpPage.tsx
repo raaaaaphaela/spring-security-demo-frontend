0be400e8c84a540d762a5567bd5306185cc85972
Raphaela <raphaela.dohrmann@gmail.com>
2023-01-18 16:02:32 +0100

    feat: signup

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,8 +1,80 @@
+import React, {FormEvent, useCallback, useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import axios from "axios";
+
 export default function SignUpPage () {
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: ""
+    });
+
+    const [errors, setErrors] = useState<string[]>([]);
+
+    const handleChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const {name, value} = event.target;
+            setCredentials({...credentials, [name]: value});
+        },
+        [credentials, setCredentials]
+    );
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const signUp = useCallback(
+        async (e: FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+
+            setErrors([]);
+
+            try {
+                await axios.post("/api/app-users", credentials);
+                navigate("/login" + location.search);
+            } catch (e) {
+                setErrors((errors) => [
+                    ...errors,
+                    "Invalid user data"
+                ]);
+            }
+        },
+        [credentials, navigate, location]
+    );
+
 
     return (
-        <div>
-            Signup
+        <div className="SignUpPage">
+            <h1>Sign Up</h1>
+
+            {errors.length > 0 && (
+                <div>
+                    {errors.map((error) => <p key={error}>{error}</p>)}
+                </div>
+            )}
+
+            <form onSubmit={signUp}>
+                <div>
+                    <input
+                        placeholder={"username"}
+                        value={credentials.username}
+                        name={"username"}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        placeholder={"password"}
+                        type={"password"}
+                        name={"password"}
+                        value={credentials.password}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </form>
         </div>
     )
 }
